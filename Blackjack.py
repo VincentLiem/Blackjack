fbc297b60fb3176a28e17930733c04d15f192659
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-10 20:15:42 -0800

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -7,21 +7,6 @@ deck = (
 '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
 )
 current_deck = list(deck)
-card_points = {
-  '2': 2,
-  '3': 3,
-  '4': 4,
-  '5': 5,
-  '6': 6,
-  '7': 7,
-  '8': 8,
-  '9': 9,
-  '10': 10,
-  'J': 10,
-  'Q': 10,
-  'K': 10,
-  'A': 11,
-}
 def draw():
     x = random.choice(current_deck)
     current_deck.remove(x)
@@ -30,6 +15,8 @@ def shuffle():
     current_deck = list(deck)
 def count_total(x):
     for card in x:
+        if card == 'J' or card == 'Q' or card == 'K':
+            card = 10
         total = 0
         total += int(card)
         return total
@@ -40,13 +27,30 @@ dealer_total = 0
 player_hand.append(draw())
 player_hand.append(draw())
 print ('Player has' + str(player_hand))
-count_total(player_hand)
-print ('Player has' + str(player_hand))
+player_total = count_total(player_hand)
 dealer_hand.append(draw())
 print ('Dealer has' + str(dealer_hand))
-count_total(dealer_hand)
-action = input('Hit or stay?')
-
-
+dealer_total = count_total(dealer_hand)
+stop = False
+while player_total < 21 and stop == False:
+    action = input('Hit or stay? ')
+    if action == 'hit' or 'Hit':
+        player_hand.append(draw())
+        print ('Player has' + str(player_hand))
+        player_total = count_total(player_hand)
+    else:
+        stop = True
+if player_total > 21:
+    print ('Bust')
+else:
+    while dealer_total < 17:
+        dealer_hand.append(draw())
+        dealer_total = count_total(dealer_hand)
+    if player_total > dealer_total:
+            print ('You win')
+    if player_total < dealer_total:
+            print ('You lose')
+    if player_total == dealer_total:
+            print ('Push')
     
 
