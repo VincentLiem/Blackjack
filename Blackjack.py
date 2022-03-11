5658ad41cd1d5f908797deb4d3edefd75a224c96
VincentLiem <96896686+VincentLiem@users.noreply.github.com>
2022-03-10 20:32:04 -0800

    Update Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,4 +1,4 @@
-
+## to do A = 1 or 11, make stay stop hitting
 import random
 deck = (
 '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A', 
@@ -6,7 +6,7 @@ deck = (
 '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
 '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A',
 )
-current_deck = list(deck)
+
 def draw():
     x = random.choice(current_deck)
     current_deck.remove(x)
@@ -14,30 +14,38 @@ def draw():
 def shuffle():
     current_deck = list(deck)
 def count_total(x):
+    total = 0
     for card in x:
         if card == 'J' or card == 'Q' or card == 'K':
             card = 10
-        total = 0
+        if card == 'A':
+            card = 1
         total += int(card)
-        return total
+    return total
+
 player_hand = []
 dealer_hand = []
 player_total = 0
 dealer_total = 0
+current_deck = list(deck)
 player_hand.append(draw())
 player_hand.append(draw())
-print ('Player has' + str(player_hand))
+print ('Player has ' + str(player_hand))
 player_total = count_total(player_hand)
+print ('Player point total is ' + str(player_total))
 dealer_hand.append(draw())
-print ('Dealer has' + str(dealer_hand))
+print ('Dealer has ' + str(dealer_hand))
 dealer_total = count_total(dealer_hand)
+print ('Dealer point total is ' + str(dealer_total))
 stop = False
+action = ''
 while player_total < 21 and stop == False:
     action = input('Hit or stay? ')
     if action == 'hit' or 'Hit':
         player_hand.append(draw())
         print ('Player has' + str(player_hand))
         player_total = count_total(player_hand)
+        print ('Player point total is ' + str(player_total))
     else:
         stop = True
 if player_total > 21:
@@ -46,6 +54,8 @@ else:
     while dealer_total < 17:
         dealer_hand.append(draw())
         dealer_total = count_total(dealer_hand)
+        print ('Dealer has ' + str(dealer_hand))
+        print ('Dealer point total is ' + str(dealer_hand))
     if player_total > dealer_total:
             print ('You win')
     if player_total < dealer_total:
