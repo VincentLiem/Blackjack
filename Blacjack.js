e70944ad8588cf3bfa36ff03c5bfe1c5c1539612
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-30 01:18:47 -0700

    Update Blacjack.js

--- Blacjack.js
+++ Blacjack.js
@@ -1,6 +1,14 @@
 function hit
-{ }
-
+{
+    let drawn = current_deck.random()
+    player_hand.push(drawn)
+    current_deck.delete_at(current_deck.index(drawn))
+}
 function stay
-{ }
+{
+    for (card in player_hand) { }
+}
 function new_bet(amount) { }
+
+const deck = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+let current_deck
