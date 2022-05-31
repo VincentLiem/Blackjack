03d55fb80a1e57a784d2a9dddb1365c55be34e79
Vincent Liem <96896686+VincentLiem@users.noreply.github.com>
2022-05-31 10:47:21 -0700

    Update Blackjack.js

--- Blackjack.js
+++ Blackjack.js
@@ -1,14 +1,23 @@
-function hit
-{
+function hit() {
     let drawn = current_deck.random()
     player_hand.push(drawn)
     current_deck.delete_at(current_deck.index(drawn))
+    document.getElementById("player_hand").value += drawn
 }
-function stay
-{
+function stay() {
     for (card in player_hand) { }
 }
-function new_bet(amount) { }
+function new_bet() {
 
-const deck = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2", "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
-let current_deck
+    current_deck = deck
+    bet = document.getElementById("bet_amount").value
+    console.log("Bet Amount: " + bet);
+}
+const deck = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2",
+    "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2",
+    "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2",
+    "A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
+    let current_deck = deck
+    let bet = 0
+console.log("Current Deck: " + current_deck);
+console.log("Bet Amount: " + bet);
